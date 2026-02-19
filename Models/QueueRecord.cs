82cda7bcf5b04fdce9cac2e96e4777120ebd9bf0
monika.kamble <monika.kamble@MUM02L13220>
2026-02-19 19:37:49 +0530

    Add project files.

---
+++ Models/QueueRecord.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MyWebApp.Models
+{
+    public class QueueRecord
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string QueueName { get; set; } = null!;
+        public string DocumentType { get; set; } = null!;
+
+        [BsonElement("ActiveQueues")]
+        public int ActiveQueues { get; set; }
+        public int AssignedDocuments { get; set; }
+    }
+}
